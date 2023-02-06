c160a41c594e376a4331144a5b1b7f779820af30
Yashwant <kumar.eryashwant@gmail.com>
2023-02-06 15:42:57 +0530

    Update main.tf

--- challenge-1/main.tf
+++ challenge-1/main.tf
@@ -2,14 +2,14 @@ provider "openshift" {
   version = "3.11.0"
 }
 
-resource "openshift_project" "example_project" {
-  display_name = "Example Project"
+resource "openshift_project" "my_project" {
+  display_name = "Project for mediawiki"
   description = "A project for deploying MediaWiki and MySQL on OpenShift"
 }
 
 resource "openshift_mysql_database_service" "mysql" {
   name            = "mysql"
-  project         = openshift_project.example_project.name
+  project         = openshift_project.my_project.name
   database_name   = "mediawiki"
   username        = "mediawiki"
   password        = "password"
@@ -18,7 +18,7 @@ resource "openshift_mysql_database_service" "mysql" {
 
 resource "openshift_mediawiki_app" "mediawiki" {
   name            = "mediawiki"
-  project         = openshift_project.example_project.name
+  project         = openshift_project.my_project.name
   database_host   = openshift_mysql_database_service.mysql.name
   database_name   = "mediawiki"
   database_user   = "mediawiki"
@@ -27,10 +27,10 @@ resource "openshift_mediawiki_app" "mediawiki" {
 
 resource "openshift_route" "mediawiki" {
   name            = "mediawiki"
-  project         = openshift_project.example_project.name
+  project         = openshift_project.my_project.name
   service         = openshift_mediawiki_app.mediawiki.name
   path            = "/"
-  host            = "www.example.com"
+  host            = "www.my_project.com"
   tls {
     termination = "edge"
   }
